3aca743747737e1696202961d6a420d093635f6a
MForever78 <shunjian1128@gmail.com>
2015-12-27 21:53:27 +0800

    Add write operate

--- client.js
+++ client.js
@@ -1,55 +1,123 @@
 var SerialPort = require('serialport').SerialPort;
 var argv = require('minimist')(process.argv.slice(2));
 var fs = require('fs');
+var path = require('path');
 
+var fd;
 
-var file;
-
-try {
-  file = fs.readFileSync(argv.f);
-} catch(err) {
+if (!argv.f) {
   console.log("Usage: node client.js -f <image file>");
   process.exit(-1);
+} else {
+  try {
+    fd = fs.openSync(path.join(__dirname, argv.f), 'r+');
+  } catch(err) {
+    console.log("Usage: node client.js -f <image file>");
+    process.exit(-1);
+  }
 }
-
-//require("serialport").list(function (err, ports) {
-//  ports.forEach(function(port) {
-//    console.log(port.comName);
-//  });
-//});
 
 var sp = new SerialPort("/dev/cu.usbserial", {
   baudrate: 115200
 });
 
 var buf = new Buffer(512);
-for (var i = 0; i < 512; i++) {
-  buf.writeInt8(i % 128, i);
-}
+var state = "idle";
+var dataCount = 0;
+var instruction = [];
+var received = [];
 
-function writeAndDrain(buffer, callback) {
-  sp.write(buffer, function(err, result) {
-    if (err) console.log(err);
-    sp.drain(callback(buffer, callback));
-  });
-}
-
-var i = 0;
 sp.on("open", function() {
   console.log("Port has open...");
-  //writeAndDrain(buf, writeAndDrain);
-
-  setInterval(function() {
-    var buffer = new Buffer(1);
-    buffer.writeInt8(i);
-    i = (i + 1) % 128;
-    sp.write(buffer, function(err) {
-      if (err) console.log(err);
-    });
-  }, 1000);
 
   sp.on('data', function(data) {
     console.log("Data received:", data.toJSON());
+    switch(state) {
+      case "idle":
+        dataCount += data.length;
+        instruction.push(data);
+        if (dataCount === 4) {
+          dataCount = 0;
+          state = "transfering";
+          
+          // decode instruction
+          console.log(instruction);
+          var operate = Buffer.concat(instruction, 4).readUInt8(3);
+          var address = Buffer.concat(instruction, 4).readUInt32LE() << 1 >> 1;
+
+          // decode operate
+          operate = operate == 128 ? "write" : "read";
+          console.log("Operate:", operate);
+          console.log("Address:", address);
+
+          if (operate === "write") {
+            sayHello();
+          } else {
+            var data = new Buffer(512);
+            fs.read(fd, data, address, 512, 0, writeToSerial);
+          }
+
+          // clear instruction
+          instruction = [];
+        }
+        break;
+      
+      case "transfering":
+        state = "idle";
+        console.log("Read: Got goodbye:", data, "\n");
+        break;
+
+      case "receiving":
+        received.push(data);
+        dataCount += data.length;
+
+        if (dataCount === 513) {
+          dataCount = 0;
+          console.log("Write: Data receive complete");
+          console.log("Write: Received goodbye");
+
+          var data = Buffer.concat(received, 512);
+          fs.write(fd, data, 0, 512, address * 512, function(err) {
+            if (err) throw err;
+            console.log("Write: Write to disk complete\n");
+            state = "idle";
+          });
+
+          received = [];
+        }
+        break;
+    }
   });
+});
+
+function writeToSerial(err, length, data) {
+  if (err) throw err;
+  var hello = new Buffer(1);
+  hello[0] = 0xff;
+  sp.write(hello, function(err, result) {
+    if (err) throw err;
+    console.log("Read: Say hello");
+
+    sp.write(data, function(err, result) {
+      if (err) throw err;
+      console.log("Read: Sent", result, "byte data");
+    });
+  });
+}
+
+function sayHello() {
+  var hello = new Buffer(1);
+  hello[0] = 0xff;
+  sp.write(hello, function(err, result) {
+    if (err) throw err;
+    state = "receiving";
+    console.log("Write: Say hello");
+  })
+}
+
+process.on('SIGINT', function() {
+  console.log("\nRest in peace, bro!");
+  fs.closeSync(fd);
+  process.exit(0);
 });
 
